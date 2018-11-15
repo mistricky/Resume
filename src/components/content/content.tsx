c4a9d789e9423d26519f88a18fc7f10e8b75ae35
HaoDaWang <1306036576@qq.com>
2018-11-15 08:37:58 +0800

    Add content component

---
+++ src/components/content/content.tsx
@@ -0,0 +1,17 @@
+import React, { Component, ReactNode } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  flex: 3;
+
+  @media (max-width: 960px) {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export class Content extends Component {
+  render(): ReactNode {
+    return <Wrapper />;
+  }
+}
