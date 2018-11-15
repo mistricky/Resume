e5eb26bd8893f42255b4bb737abc4ff22bfa40c8
HaoDaWang <1306036576@qq.com>
2018-11-15 08:40:14 +0800

    Add root module

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,18 +1,60 @@
 import React, { Component, ReactNode } from "react";
 import { ThemeWrapper, injectGlobal, theme } from "src/theme";
-import { Resume } from "src/components/resume";
+import styled from "src/theme/style";
+import { Side } from "src/components/side";
+import { Content } from "src/components/content";
+import { HeaderBar } from "src/components/header";
 
 injectGlobal`
-  body{
-    background:${theme.backgroundColor}
+  * {
+    box-sizing:border-box;
   }
+
+  html, body {
+    height: 100%;
+    margin: 0;
+  }
+
+  html {
+    font-size:12px;
+  }
+
+  body{
+    position:relative;
+    background: ${theme.backgroundColor};
+    height:100%;
+    padding-top:4.5rem;
+    padding: 0rem 1rem;
+  }
+
+  #root {
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (min-width: 756px) {
+    #root {
+      height: 100%;
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
 `;
 
 export class App extends Component {
   render(): ReactNode {
     return (
       <ThemeWrapper>
-        <Resume />
+        <Wrapper>
+          <HeaderBar />
+          <Side />
+          <Content />
+        </Wrapper>
       </ThemeWrapper>
     );
   }
