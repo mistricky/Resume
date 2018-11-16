063c048535c970d40b69101954c3ff521e652dce
HaoDaWang <1306036576@qq.com>
2018-11-16 20:58:06 +0800

    Don't need PWA

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
 import { App } from "./views";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
