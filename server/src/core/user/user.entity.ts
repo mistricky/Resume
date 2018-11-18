cce053b64ceeb3da34eb5a56fe95fb897e5af4fd
HaoDaWang <1306036576@qq.com>
2018-11-18 17:10:27 +0800

    Define user module

---
+++ server/src/core/user/user.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  username: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  station: string;
+
+  @Column()
+  sex: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  location: string;
+
+  @Column()
+  telNum: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  other: string;
+}
---
+++ server/src/core/user/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+}
