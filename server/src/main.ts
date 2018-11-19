4602fce6786a8540af1f9a43ea0a90f5d741391c
HaoDaWang <1306036576@qq.com>
2018-11-19 21:59:37 +0800

    Catch error

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
 
-async function bootstrap() {
+import {AppModule} from './app.module';
+import {PromiseCatcher, logger} from './utils';
+
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
+PromiseCatcher(bootstrap());
