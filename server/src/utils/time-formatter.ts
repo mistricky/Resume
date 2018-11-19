b9cd1926b32dd9d39e9d739a9314a0e9ce60578d
HaoDaWang <1306036576@qq.com>
2018-11-19 22:16:16 +0800

    Stronger

--- server/src/utils/time-formatter.ts
+++ server/src/utils/time-formatter.ts
@@ -1,3 +1,21 @@
+function completeTime(target: number): string {
+  let st = target.toString();
+
+  return st.length === 1 ? `0${st}` : st;
+}
+
+function processYear(date: Date): string {
+  return `${date.getFullYear()}-${completeTime(date.getMonth())}-${completeTime(
+    date.getDay(),
+  )}`;
+}
+
+function processTime(date: Date): string {
+  return `${completeTime(date.getHours())}:${completeTime(
+    date.getMinutes(),
+  )}:${completeTime(date.getSeconds())}`;
+}
+
 export function timeFormat(date: Date): string {
-  return `[${date.getFullYear()}-${date.getMonth()}-${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`;
+  return `[ ${processYear(date)} ${processTime(date)} ]`;
 }
