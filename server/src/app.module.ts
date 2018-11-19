a44347adc75733af9d247784ed5ecb3328733fca
HaoDaWang <1306036576@qq.com>
2018-11-19 21:34:43 +0800

    Update ormconfig path

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,16 @@
 import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 
+import * as database from '../.configs/database.json';
+
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 // tslint:disable-next-line:import-path-shallowest
 import {UserModule} from './core/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(database as TypeOrmModule), UserModule],
   controllers: [AppController],
   providers: [AppService],
-  modules: [UserModule],
 })
 export class AppModule {}
