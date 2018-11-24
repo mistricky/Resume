806ff6ee379e6c827a487469c6a726c89beaa772
HaoDaWang <1306036576@qq.com>
2018-11-24 21:20:34 +0800

    Mov

---
+++ shared/src/library/types/promote.d.ts
@@ -0,0 +1,9 @@
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { IconDefinition } from "@fortawesome/fontawesome-common-types";
+
+// declare module "@fortawesome/react-fontawesome" {
+//   export interface Props extends React.DOMAttributes<any> {
+//     icon: any;
+//     mask?: IconDefinition;
+//   }
+// }
--- shared/types/promote.d.ts
+++
@@ -1,9 +0,0 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconDefinition } from "@fortawesome/fontawesome-common-types";
-
-declare module "@fortawesome/react-fontawesome" {
-  export interface Props extends React.DOMAttributes<any> {
-    // icon: any;
-    // mask?: IconDefinition;
-  }
-}
