2de23779e65bb01d9ac15887e32f40ed37f99311
HaoDaWang <1306036576@qq.com>
2018-11-28 00:03:46 +0800

    Add bottom-modal component

---
+++ client/src/ui/bottom-modal/bottom-modal.tsx
@@ -0,0 +1,45 @@
+import React, {Component, ReactNode} from 'react';
+import styled, {keyframes} from 'styled-components';
+
+interface BottomModalProps {
+  isView?: boolean;
+}
+
+const Pop = keyframes`
+  from {
+    bottom: -100px;
+  }
+
+  to {
+    bottom: 0px;
+  }
+`;
+
+const Hidden = keyframes`
+  from {
+    bottom: 0px;
+  }
+
+  to {
+    bottom: -100px;
+  }
+`;
+
+const Wrapper = styled.div<BottomModalProps>`
+  width: 100%;
+  height: 100px;
+  position: absolute;
+  left: 0px;
+  bottom: -100px;
+  background: #000;
+  animation-fill-mode: forwards !important;
+  animation: ${Pop} 1s linear 1, ${Hidden} 1s linear 3s 1;
+`;
+
+export class BottomModal extends Component<BottomModalProps> {
+  render(): ReactNode {
+    let {isView = false} = this.props;
+
+    return <Wrapper isView={isView} />;
+  }
+}
