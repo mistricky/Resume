024a066a7fe33744bc8f415e51d6c7c59568bfbc
HaoDaWang <1306036576@qq.com>
2018-11-28 11:29:48 +0800

    Add props

--- client/src/components/login-prompt-modal/basic-button.tsx
+++ client/src/components/login-prompt-modal/basic-button.tsx
@@ -3,20 +3,39 @@ import React, {Component, ReactNode} from 'react';
 import styled, {theme} from 'src/theme/style';
 import {Button} from 'src/ui';
 
-const BasicButton = styled(Button)`
-  width: 100px;
+interface BasicButtonProps {
+  width?: string;
+}
+
+interface BasicButtonWrapperProps extends BasicButtonProps {
+  onClick?(): void;
+}
+
+const BasicButton = styled(Button)<BasicButtonProps>`
+  width: ${props => props.width || '100px'};
   height: 50px;
+  margin-left: 16px;
+  ${props => (props.width ? 'padding: 0 15px;' : undefined)}
+
+  @media (min-width: ${props => props.theme.mobileWidth}) {
+    & {
+      margin-left: 20px;
+      margin-right: 6px;
+    }
+  }
 `;
 
-export class BasicButtonWrapper extends Component {
+export class BasicButtonWrapper extends Component<BasicButtonWrapperProps> {
   render(): ReactNode {
-    let {children} = this.props;
+    let {children, width, onClick} = this.props;
 
     return (
       <BasicButton
         bgColor={theme.green}
         hoverColor={theme.deepGreen}
         color="#fff"
+        width={width}
+        onClick={onClick}
       >
         {children}
       </BasicButton>
