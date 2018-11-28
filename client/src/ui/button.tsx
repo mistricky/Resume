6bb171453801a8f908b5b969d94f30bf233def1b
HaoDaWang <1306036576@qq.com>
2018-11-28 10:32:13 +0800

    Update color

--- client/src/ui/button.tsx
+++ client/src/ui/button.tsx
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
   outline: none;
+  color: white;
   cursor: pointer;
+  border:none;
   background:${props => props.theme.green}
 
   &:hover {
-    background:${props => props.theme.hoverColor}
+    background:${props => props.theme.deepGreen}
   }
 `;
