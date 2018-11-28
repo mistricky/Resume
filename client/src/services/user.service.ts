7726ed5d5804c76f8ba3110cbc3c29ed84ba28e5
HaoDaWang <1306036576@qq.com>
2018-11-28 20:35:21 +0800

    Add avatar

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -3,4 +3,8 @@ import {observable} from 'mobx';
 export class UserService {
   @observable
   user!: string;
+
+  @observable
+  avatar: string =
+    'https://avatars1.githubusercontent.com/u/22574136?s=400&u=03105b1c3ce6786a1ec29d293d9dbea77909605f&v=4';
 }
