409c97d656e73017a9818782a5072f428bd12773
HaoDaWang <1306036576@qq.com>
2018-11-28 16:58:26 +0800

    Add register

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,5 +2,6 @@ export * from './content';
 export * from './header';
 export * from './login';
 export * from './login-prompt-modal';
+export * from './register';
 export * from './resume';
 export * from './side';
---
+++ client/src/components/register/register.tsx
@@ -0,0 +1,7 @@
+import React, {Component, ReactNode} from 'react';
+
+export class Register extends Component {
+  render(): ReactNode {
+    return <div />;
+  }
+}
