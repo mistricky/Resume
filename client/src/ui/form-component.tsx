a36ef4fcca9140b8c4c8e078a5b33c44205e8173
HaoDaWang <1306036576@qq.com>
2018-11-29 20:30:02 +0800

    Wrap form component

---
+++ client/src/ui/form-component.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const FormComponent = styled.div`
+  padding: 10px 0;
+  margin-top: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
