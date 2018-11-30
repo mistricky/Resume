735442574cddff88747e015761cf8bdd81b94d4b
HaoDaWang <1306036576@qq.com>
2018-11-30 22:21:48 +0800

    The component for text that like logo

--- client/src/components/tag-text.tsx
+++ client/src/components/tag-text.tsx
@@ -3,7 +3,9 @@ import {Dict} from 'shared';
 import styled from 'styled-components';
 
 interface TagTextProps {
+  text: string;
   position?: 'left' | 'right';
+  description?: string;
 }
 
 interface TagTextWrapperProps {
@@ -16,24 +18,44 @@ const PositionDict: Dict<'flex-start' | 'flex-end'> = {
 };
 
 const Wrapper = styled.div<TagTextWrapperProps>`
-  width: '100%';
+  width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: ${props => props.parsedPosition};
+  margin-top: 3rem;
+`;
+
+const TextWrapper = styled.div`
+  width: fit-content;
+
+  > p {
+    margin: 0;
+    padding: 0;
+    line-height: 40px;
+  }
 `;
 
 const Text = styled.p`
   font-size: ${props => props.theme.tagTextFont};
 `;
 
+const Description = styled.p`
+  text-align: center;
+  font-size: ${props => props.theme.mediumFont};
+`;
+
 export class TagText extends Component<TagTextProps> {
   render(): ReactNode {
-    let {position = 'left', children} = this.props;
+    let {position = 'left', text, description} = this.props;
 
     let parsedPosition = PositionDict[position];
 
     return (
       <Wrapper parsedPosition={parsedPosition}>
-        <Text>{children}</Text>
+        <TextWrapper>
+          <Text>{text}</Text>
+          <Description>{description}</Description>
+        </TextWrapper>
       </Wrapper>
     );
   }
