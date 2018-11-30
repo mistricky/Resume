30a66db276755695774f15e2af33837bfe59f9b3
HaoDaWang <1306036576@qq.com>
2018-11-30 22:25:07 +0800

    Enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import {NestFactory} from '@nestjs/core';
 
+import * as CorsOptions from '../.configs/cors.json';
+
 import {AppModule} from './app.module';
 import {PromiseCatcher} from './utils';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors(CorsOptions);
+  await app.listen(3200);
 }
 
 PromiseCatcher(bootstrap());
