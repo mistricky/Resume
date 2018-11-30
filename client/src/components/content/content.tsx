9a4fb754a3d63c7eafa9bdfb14150d336e972477
HaoDaWang <1306036576@qq.com>
2018-11-30 22:21:59 +0800

    Update style

--- client/src/components/content/content.tsx
+++ client/src/components/content/content.tsx
@@ -1,17 +1,29 @@
-import React, { Component, ReactNode } from "react";
-import styled from "styled-components";
+import React, {Component, ReactNode} from 'react';
+import styled from 'styled-components';
+
+import {userService} from 'src/entrances';
+
+import {TagText} from '../tag-text';
 
 const Wrapper = styled.div`
   flex: 1;
+  padding: 0 2rem;
 
   @media (max-width: 960px) {
     width: 100%;
     height: 100%;
+    padding: 5rem 1rem;
   }
 `;
 
 export class Content extends Component {
   render(): ReactNode {
-    return <Wrapper />;
+    let {user, description} = userService;
+
+    return (
+      <Wrapper>
+        <TagText text={user} description={description} />
+      </Wrapper>
+    );
   }
 }
