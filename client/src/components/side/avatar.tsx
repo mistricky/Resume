e3bc5016c1174799ce5d75d615eb9eccd68f9b68
HaoDaWang <1306036576@qq.com>
2018-12-01 16:20:07 +0800

    Extends avatar

--- client/src/components/side/avatar.tsx
+++ client/src/components/side/avatar.tsx
@@ -1,32 +1,45 @@
 import React, {Component, ReactNode} from 'react';
+import {Dict} from 'shared';
 import styled from 'styled-components';
+
+type Size = 'small' | 'large';
 
 interface AvatarProps {
   src: string;
+  size?: Size;
 }
 
-const Wrapper = styled.div`
-  width: 15rem;
-  min-height: 15rem;
+interface AvatarWrapperProps {
+  size: string;
+}
+
+const SizeDict: Dict<string> = {
+  small: '3rem',
+  large: '15rem',
+};
+
+const Wrapper = styled.div<AvatarWrapperProps>`
+  width: ${props => props.size};
+  min-height: ${props => props.size};
   border-radius: 50%;
   background: ${props => props.theme.backgroundColor};
-  /* display: flex; */
-  /* justify-content: center; */
-  /* align-items: center; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
   overflow: hidden;
 `;
 
 const AvatarBody = styled.img`
-  width: 15rem;
-  height: 15rem;
+  width: 100%;
+  height: 100%;
 `;
 
 export class Avatar extends Component<AvatarProps> {
   render(): ReactNode {
-    let {src} = this.props;
+    let {src, size = 'large'} = this.props;
 
     return (
-      <Wrapper>
+      <Wrapper size={SizeDict[size]}>
         <AvatarBody src={src} />
       </Wrapper>
     );
