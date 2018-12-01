124f5efe30d40a381b22515020177eb2847bbfe3
HaoDaWang <1306036576@qq.com>
2018-12-01 21:36:44 +0800

    Add project experiences

--- client/src/entrances/services.ts
+++ client/src/entrances/services.ts
@@ -1,5 +1,7 @@
 import {LoginService, UserService, WorkExperienceService} from 'src/services';
+import {ProjectExperienceService} from 'src/services/project-experience.service';
 
 export const userService = new UserService();
 export const loginService = new LoginService();
 export const workExperienceService = new WorkExperienceService();
+export const projectExperienceService = new ProjectExperienceService();
---
+++ client/src/services/project-experience.service.ts
@@ -0,0 +1,35 @@
+import {observable} from 'mobx';
+
+interface ProjectExperience {
+  company: string;
+  projectName: string;
+  description: string;
+}
+
+export class ProjectExperienceModel {
+  @observable
+  company: string;
+
+  @observable
+  projectName: string;
+
+  @observable
+  description: string;
+
+  constructor({company, projectName, description}: ProjectExperience) {
+    this.company = company;
+    this.projectName = projectName;
+    this.description = description;
+  }
+}
+
+export class ProjectExperienceService {
+  @observable
+  projectExperiences: ProjectExperienceModel[] = [];
+
+  setProjectExperiences(projectExperiences: ProjectExperience[]): void {
+    projectExperiences.map(
+      experience => new ProjectExperienceModel(experience),
+    );
+  }
+}
