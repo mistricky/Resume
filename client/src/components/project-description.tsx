eb20c0a9c7bdd42a3c10f0eb43f1b8758861aecf
HaoDaWang <1306036576@qq.com>
2018-12-01 21:25:49 +0800

    Add project-description component

---
+++ client/src/components/project-description.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ProjectDescription = styled.div`
+  background: ${props => props.theme.shadowColor};
+  border-radius: ${props => props.theme.borderRadius};
+  padding: 1rem;
+`;
