2ec1024d3025367a8b8bf4558205b550327dc899
HaoDaWang <1306036576@qq.com>
2018-12-01 21:37:09 +0800

    Add hasLine props

--- client/src/ui/title.tsx
+++ client/src/ui/title.tsx
@@ -1,16 +1,34 @@
+import React, {Component, ReactNode} from 'react';
 import styled from 'styled-components';
 
-export const Title = styled.h2`
-  position: relative;
+export interface TitleProps {
+  hasLine?: boolean;
+}
+
+export const TitleBody = styled.h2<TitleProps>`
+  position: ${props => (props.hasLine ? 'relative' : 'static')};
   margin-top: 1.5rem;
 
-  ::after {
-    content: '';
-    width: 100%;
-    height: 1px;
-    border-bottom: 1px solid ${props => props.theme.darkGray};
-    position: absolute;
-    top: calc(100% + 1rem);
-    left: 0;
-  }
+  ${props =>
+    props.hasLine
+      ? `
+    ::after {
+      content: '';
+      width: 100%;
+      height: 1px;
+      border-bottom: 1px solid ${props.theme.darkGray};
+      position: absolute;
+      top: calc(100% + 1rem);
+      left: 0;
+    }
+  `
+      : undefined}
 `;
+
+export class Title extends Component<TitleProps> {
+  render(): ReactNode {
+    let {hasLine = true, children} = this.props;
+
+    return <TitleBody hasLine={hasLine}>{children}</TitleBody>;
+  }
+}
