d138ae4342f4900066d41586fe215ffd45cac87c
HaoDaWang <1306036576@qq.com>
2018-12-02 14:23:16 +0800

    Add highlight.css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import 'antd/dist/antd.css';
 import 'font-awesome/css/font-awesome.css';
+import 'highlight.js/styles/github.css';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
