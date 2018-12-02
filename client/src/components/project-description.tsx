d6e09f8550bd7f53ee1b16604ec08f427407edcb
HaoDaWang <1306036576@qq.com>
2018-12-02 15:49:24 +0800

    For view description of project

--- client/src/components/project-description.tsx
+++ client/src/components/project-description.tsx
@@ -1,21 +1,37 @@
 import React, {Component, ReactNode} from 'react';
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+import {markdown} from 'src/utils';
+
+interface ProjectDescriptionProps {
+  code: string;
+}
+
+export const Wrapper = styled.div`
+  & code {
+    border-radius: ${props => props.theme.borderRadius};
+  }
+`;
 
 export const ProjectDescriptionBody = styled.div`
-  background: ${props => props.theme.shadowColor};
+  background: ${props => props.theme.shallowGray};
   border-radius: ${props => props.theme.borderRadius};
   padding: 1rem;
 `;
 
-export class ProjectDescription extends Component {
+export class ProjectDescription extends Component<ProjectDescriptionProps> {
   render(): ReactNode {
-    let {children} = this.props;
+    let {code} = this.props;
+
+    const renderedCode = markdown(code);
 
     return (
       <Wrapper>
-        <ProjectDescriptionBody />
+        <ProjectDescriptionBody
+          dangerouslySetInnerHTML={{
+            __html: renderedCode,
+          }}
+        />
       </Wrapper>
     );
   }
