2a7d2621b5ce6b9aef1bc6eca04f5e3d34abfc8f
HaoDaWang <1306036576@qq.com>
2018-12-02 15:48:08 +0800

    Add marked

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,13 @@ import 'antd/dist/antd.css';
 import 'font-awesome/css/font-awesome.css';
 import 'highlight.js/styles/github.css';
 
+import highlight from 'highlight.js';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import {App} from './views';
+
+highlight.initHighlightingOnLoad();
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 export * from './services';
