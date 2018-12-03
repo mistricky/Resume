325d3fae06f3ca7a77aaf926b7aef162ff60c1bd
HaoDaWang <1306036576@qq.com>
2018-12-03 09:47:06 +0800

    Add related config for graphql

--- client/src/constant.ts
+++ client/src/constant.ts
@@ -1,4 +1,8 @@
+// request address config
 export const BASE_URL = 'http://localhost';
 export const PORT = '3200';
 
 export const REQUEST_BASE_URL = `${BASE_URL}:${PORT}`;
+
+// graphql
+export const GRAPHQL_ADDRESS = `${REQUEST_BASE_URL}/graphql`;
