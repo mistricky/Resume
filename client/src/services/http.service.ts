670101d1828a64eb36b81e9a9f7c23258bf94bcf
HaoDaWang <1306036576@qq.com>
2018-12-03 09:07:18 +0800

    Wrap request api

--- client/src/services/http.service.ts
+++ client/src/services/http.service.ts
@@ -4,4 +4,44 @@ import {REQUEST_BASE_URL} from 'src/constant';
 
 Axios.defaults.baseURL = REQUEST_BASE_URL;
 
-export class HTTP {}
+interface BaseRequestOptions {
+  type: string;
+}
+
+type Method = 'Get' | 'Post';
+
+export class HTTPService {
+  post<T>(
+    path: string,
+    body: T,
+    baseRequestOptions?: BaseRequestOptions,
+  ): Promise<T> {
+    return this.baseRequest(path, 'Post', body, baseRequestOptions);
+  }
+
+  get<T>(path: string): Promise<T> {
+    return this.baseRequest(path, 'Get');
+  }
+
+  private async baseRequest<T>(
+    path: string,
+    method: Method,
+    body?: T,
+    baseRequestOptions?: BaseRequestOptions,
+  ): Promise<T> {
+    let options = baseRequestOptions || {
+      type: 'application/json;charset=utf-8',
+      timeout: 10 * 1000,
+    };
+
+    let result!: T;
+
+    try {
+      result = await Axios[method](path, body, options);
+    } catch (e) {
+      console.error(e);
+    }
+
+    return result;
+  }
+}
