5dcf23a791c3ee007dab347baf008aed09d0b5cc
HaoDaWang <1306036576@qq.com>
2018-12-05 16:39:15 +0800

    Add entry button component

---
+++ client/src/components/entry-btn.tsx
@@ -0,0 +1,44 @@
+import {Button} from 'antd';
+import React, {Component, ReactNode} from 'react';
+
+import styled, {theme} from 'src/theme/style';
+
+interface ActionButtonBodyProps {
+  bgColor?: string;
+  color?: string;
+  hoverColor?: string;
+}
+
+export interface EntryBtnProps extends ActionButtonBodyProps {
+  loading: boolean;
+}
+
+export const ActionButtonBody = styled.div<ActionButtonBodyProps>`
+  > button {
+    background: ${props => props.bgColor};
+    color: ${props => props.color};
+    transition: 0.3s all;
+  }
+
+  > button:hover {
+    background: ${props => props.hoverColor};
+  }
+`;
+
+export class ActionButton extends Component<EntryBtnProps> {
+  render(): ReactNode {
+    let {
+      children,
+      loading,
+      bgColor,
+      color = '#000',
+      hoverColor = theme.shadowColor,
+    } = this.props;
+
+    return (
+      <ActionButtonBody bgColor={bgColor} color={color} hoverColor={hoverColor}>
+        <Button loading={loading}>{children}</Button>
+      </ActionButtonBody>
+    );
+  }
+}
