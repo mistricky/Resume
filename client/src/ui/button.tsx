39ea12eb4af55606c4790a34f8ed697ed8f898ca
HaoDaWang <1306036576@qq.com>
2019-01-08 13:28:24 +0800

    Format

--- client/src/ui/button.tsx
+++ client/src/ui/button.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-interface ButtonProps {
+export interface ButtonProps {
   bgColor: string;
   color: string;
   hoverColor: string;
@@ -10,11 +10,12 @@ export const Button = styled.button<ButtonProps>`
   outline: none;
   color: ${props => props.color};
   cursor: pointer;
-  border:none;
+  border: none;
   border-radius: ${props => props.theme.borderRadius};
-  background:${props => props.bgColor}
+  background: ${props => props.bgColor};
+  transition: 0.3 background;
 
   &:hover {
-    background:${props => props.hoverColor}
+    background: ${props => props.hoverColor};
   }
 `;
